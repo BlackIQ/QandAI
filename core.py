9b3508564bb710efb4b9211d3fa7f88038faef2c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-18 21:32:36 +0330

    Update to load file

--- core.py
+++ core.py
@@ -1,23 +1,35 @@
 import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+import joblib
 
-# Load the dataset using pandas
-df = pd.read_json('faq.json')
+# Load the JSON data into a Pandas dataframe
+faq_df = pd.read_json("faq_data.json")
 
-# Vectorize the patterns
 # Preprocess the data
+faq_df["Question"] = faq_df["Question"].str.lower()  # convert to lowercase
+
+# Vectorize the questions
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(df['question'])
-y = df['answer']
+X = vectorizer.fit_transform(faq_df["Question"])
+y = faq_df["Answer"]
 
-# Train the model
-model = MultinomialNB()
-model.fit(X, y)
+# Train the classifier
+clf = MultinomialNB()
+clf.fit(X, y)
+
+# Save the model to a file
+joblib.dump(clf, "faq_model.joblib")
+
+# Define a predict_answer() function
 
 
-def predict(question):
-    question_vectorized = vectorizer.transform([question])
-    predicted_answer = model.predict(question_vectorized)[0]
+def predict_answer(question):
+    # Load the model from a file
+    clf_loaded = joblib.load("faq_model.joblib")
 
-    return predicted_answer
+    question = question.lower()
+    X_question = vectorizer.transform([question])
+    answer = clf_loaded.predict(X_question)[0]
+    return answer
